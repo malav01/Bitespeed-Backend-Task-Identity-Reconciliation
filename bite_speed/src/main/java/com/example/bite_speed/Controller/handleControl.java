256dca755be1d3dad6582d34ef217eb88acd9623
malav <malav@raapid.ai>
2023-06-08 11:07:47 +0530

    Created DAO and DTO

---
+++ bite_speed/src/main/java/com/example/bite_speed/Controller/handleControl.java
@@ -0,0 +1,47 @@
+package com.example.bite_speed.Controller;
+
+import java.sql.*;
+import java.time.ZonedDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.bite_speed.dao.ContactDao;
+import com.example.bite_speed.dao.EmailDao;
+import com.example.bite_speed.dao.PhoneDao;
+import com.example.bite_speed.dto.Contact;
+import com.example.bite_speed.dto.Phone;
+import com.example.bite_speed.dto.Request;
+import com.example.bite_speed.dto.Response;
+
+@RestController
+public class handleControl {
+    @Autowired
+    public ContactDao contactDao;
+    @Autowired
+    public PhoneDao phoneDao;
+    @Autowired
+    public EmailDao emailDao;
+
+    @PostMapping(path = "/identify")
+    public Request handlRequest(@RequestBody Request input){
+        String phoneNumber = input.getPhoneNumber();
+        String email = input.getEmail();
+        
+        if(!phoneDao.existsById(phoneNumber) && !emailDao.existsById(email)){
+            Contact contact = new Contact();
+            contact.setPhoneNumber(phoneNumber);
+            contact.setEmail(email);
+            contact.setLinkedId(null);
+            contact.setLinkPrecedence("primary");
+            contact.setCreatedAt(ZonedDateTime.now());
+            contact.setDeletedAt(null);
+            contact.setUpdatedAt(ZonedDateTime.now());
+            contactDao.save(contact);
+        }
+
+        return input;
+    }
+}
